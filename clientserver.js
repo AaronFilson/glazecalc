084c6605f8929a806eba50cd762f428e1ab3abda
Aaron Filson <aaronfilson@gmail.com>
2023-11-13 23:18:36 -0800

    Have client port pick up from env.

--- clientserver.js
+++ clientserver.js
@@ -1,4 +1,4 @@
 const express = require('express');
-
+var clientPort = process.env.CLIENTPORT || 4001;
 express().use(express.static(__dirname + '/build'))
-  .listen(4001, () => console.log('Client server up on port 4001.'));
+  .listen(, () => console.log('Client server up on port' + clientPort + '.'));
