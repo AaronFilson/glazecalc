0b1b0762173953bf6c2189bac86de2117b66d3dd
Aaron Filson <aaronfilson@gmail.com>
2023-11-13 23:21:26 -0800

    Add in var to listener.

--- clientserver.js
+++ clientserver.js
@@ -1,4 +1,4 @@
 const express = require('express');
 var clientPort = process.env.CLIENTPORT || 4001;
 express().use(express.static(__dirname + '/build'))
-  .listen(, () => console.log('Client server up on port' + clientPort + '.'));
+  .listen(clientPort, () => console.log('Client server up on port' + clientPort + '.'));
